2cb8f563fecabec3f73db48a45044e2c80f8b6ed
songjg <toblessong@163.com>
2022-03-19 22:38:00 +0800

    洗代码

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ DIR_CONFIG="/etc/v2ray"
 DIR_RUNTIME="/usr/bin"
 DIR_TMP="$(mktemp -d)"
 
-# Write V2Ray configuration
+# Write configuration
 cat << EOF > ${DIR_TMP}/heroku.json
 {
     "inbounds": [{
@@ -30,7 +30,7 @@ cat << EOF > ${DIR_TMP}/heroku.json
 }
 EOF
 
-# Get V2Ray executable release
+# Get executable release
 curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/v2ray_dist.zip
 busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
 
@@ -38,9 +38,9 @@ busybox unzip ${DIR_TMP}/v2ray_dist.zip -d ${DIR_TMP}
 mkdir -p ${DIR_CONFIG}
 ${DIR_TMP}/v2ctl config ${DIR_TMP}/heroku.json > ${DIR_CONFIG}/config.pb
 
-# Install V2Ray
+# Install
 install -m 755 ${DIR_TMP}/v2ray ${DIR_RUNTIME}
 rm -rf ${DIR_TMP}
 
-# Run V2Ray
+# Run
 ${DIR_RUNTIME}/v2ray -config=${DIR_CONFIG}/config.pb
